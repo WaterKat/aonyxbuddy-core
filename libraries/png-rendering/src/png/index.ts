c848274612cdd9d396297b733b19b58144d1d758
waterkat <waterkat@aonyxlimited.com>
2023-09-18 20:59:22 +0000

    this is a test to check remote access

--- libraries/png-rendering/src/png/index.ts
+++ libraries/png-rendering/src/png/index.ts
@@ -2,3 +2,8 @@ export * from './png-state-renderer.js';
 export * from './ipng-config.js';
 
 export * as Tests from './png-config.test.js';
+
+/*
+ * This is a test to see if remote access works properly
+ */
+
