757e1074749cc0f7405d6fbb4fc866b82501af43
waterkat@wk-lpc <waterkat@aonyxlimited.com>
2023-11-28 17:36:49 -0800

    added documentation

--- src/log.ts
+++ src/log.ts
@@ -1,8 +1,15 @@
 const NAME_TAG: string = 'aonyxbuddy';
 
-type LogTags = 'log' | 'error' | 'info' | 'warn';
+/** Available Tags for use with the Log function, similar to console.log, console.warn, etc */
+type LogTag = 'log' | 'error' | 'info' | 'warn';
 
-export default function Log(tag: LogTags, ...args: any): void {
+/**
+ * Intermediary for logging to standardize logging for the app, as well as possbly add logging to remote servers
+ * @param tag the tag to identify the type of log to ouput
+ * @param args the values to be outputted
+ * @returns void
+ */
+export default function Log(tag: LogTag, ...args: any): void {
     console[tag](`[${NAME_TAG}:${tag === 'log' ? '' : `/${tag}`}]`, ...args);
     return;
 }
