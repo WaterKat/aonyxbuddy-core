fa0e5a9af83a2fd2b5af385ef59cfdac6affafac
waterkat <waterkat@aonyxlimited.com>
2024-04-14 22:14:09 -0700

    returning disconnect callback

--- src/bridge/stream-elements/listener.ts
+++ src/bridge/stream-elements/listener.ts
@@ -1,30 +1,38 @@
-import { Types } from "../../core/stream-events/index.js"; 
+import { Types } from "../../core/stream-events/index.js";
 
 import EventTranslator from "./event-translator.js";
-        //^?
+//^?
 
-export default function ListenForStreamElementsEvents(callback: (event: Types.TStreamEvent) => void): void | Error {
+export default function ListenForStreamElementsEvents(
+    callback: (event: Types.TStreamEvent) => void
+) {
     if (!window) {
         return new Error("Window not found, this can only run in a browser");
     }
 
-    window.addEventListener("onEventReceived",
-        function (_eventData: any) {
-            if (!_eventData || !_eventData.detail || !_eventData.detail.event) return;
+    function OnEvent(_eventData: any) {
+        if (!_eventData || !_eventData.detail || !_eventData.detail.event)
+            return;
 
-            if (typeof _eventData.detail.event.itemId !== "undefined") {
-                _eventData.detail.listener = "redemption-latest";
-            }
-
-            const streamElementEvent = {
-                ..._eventData.detail.event,
-                type: _eventData.detail.listener.split("-")[0]
-            }
-            //console.log("raw se: ",streamElementEvent);
-            const aonyxStreamEvent = EventTranslator(streamElementEvent);
-            if (aonyxStreamEvent) {
-                callback(aonyxStreamEvent);
-            }
+        if (typeof _eventData.detail.event.itemId !== "undefined") {
+            _eventData.detail.listener = "redemption-latest";
         }
-    );
+
+        const streamElementEvent = {
+            ..._eventData.detail.event,
+            type: _eventData.detail.listener.split("-")[0]
+        }
+        //console.log("raw se: ",streamElementEvent);
+        const aonyxStreamEvent = EventTranslator(streamElementEvent);
+        if (aonyxStreamEvent) {
+            callback(aonyxStreamEvent);
+        }
+    }
+
+    window.addEventListener("onEventReceived", OnEvent);
+
+    return {
+        RemoveListener:
+            () => window.removeEventListener("onEventReceived", OnEvent)
+    };
 }
