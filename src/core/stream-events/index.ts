a34c58d52bb4d6c9112db089abfdfe59bdc156bd
waterkat <waterkat@aonyxlimited.com>
2024-04-17 01:41:28 -0700

    removing legacy files

--- src/core/stream-events/index.ts
+++ src/core/stream-events/index.ts
@@ -3,23 +3,3 @@ export * from "./processing/index.js";
 export * from "./template.js";
 
 export * from "./custom-processing/index.js";
-
-/* 
-export * as Types from './types.js';
-//export * as Detection from './old.detection/index.js';
-//export * as Manipulation from './old.manipulation/index.js';
-
-export { 
-    TStreamEvent,
-    TChat as TChatMessage,
-    TEmote,
-    TPermissions,
-    EStreamEventType
-} from './types.js';
-
-export * from "./processing/index.js"
-export * from "./custom-processing/index.js";
-
-import { ProcessEvent } from './processing/index.js';
-export default ProcessEvent;
-*/
