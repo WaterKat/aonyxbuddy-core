192977104bb3556602789996ab179b95a4b2224c
waterkat <waterkat@aonyxlimited.com>
2024-04-17 20:49:05 -0700

    adding docs server script

---
+++ tools/serve-docs.ts
@@ -0,0 +1,18 @@
+const PORT : number = 3000;
+const HOSTNAME : string = 'localhost';
+
+const server = Bun.serve({
+    fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/") {
+            url.pathname = "index.html";
+        }
+        
+        const filePath = "./docs" + url.pathname;
+        return new Response(Bun.file(filePath));
+    },
+    port: PORT,
+    hostname: HOSTNAME
+});
+
+console.log(`Listening on ${server.hostname}:${server.port}`)
