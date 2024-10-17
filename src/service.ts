2022b18fedc252c1ef94195d46557f8f34c850ab
WaterKat <waterkat@aonyxlimited.com>
2024-10-16 17:05:06 -0700

    Renaming file for consistency

--- src/service.ts
+++ src/service.ts
@@ -5,7 +5,7 @@ import {
 import {
   StreamElementsEventsService,
   TStreamElementsEventsServiceOptions,
-} from "./events/streamelements-overlay/stream-elements-service.js";
+} from "./events/streamelements-overlay/streamelements-event-service.js";
 import {
   StreamElementsSocketService,
   TStreamElementsSocketServiceOptions,
