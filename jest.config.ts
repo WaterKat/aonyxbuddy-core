c7d28138aa9c98664e7fc6037f428b646afd3219
WaterKat <waterkat@aonyxlimited.com>
2024-10-18 21:48:52 -0700

    Changing jest config to ts

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+// jest.config.ts
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  // [...]
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
+    "^.+\\.m?[tj]sx?$": [ //to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
+      //'^.+\\.tsx?$': [
+      "ts-jest",
+      {
+        useESM: true,
+        isolatedModules: true,
+        tsconfig: "<rootDir>/tsconfig.jest.json",
+      },
+    ],
+  },  
+};
+
+export default jestConfig;
