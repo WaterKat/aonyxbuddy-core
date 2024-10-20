6ef8398f92fe2a63ddc1a7c10de4bd3ad4a2422b
WaterKat <waterkat@aonyxlimited.com>
2024-10-20 00:11:44 -0700

    Adding conditional code based on environment

--- src/service.app.ts
+++ src/service.app.ts
@@ -1,27 +1,58 @@
 import { ObjectContainsKey } from "./lib.js";
 import { AonyxBuddyWebClient, TAonyxBuddyWebClientOptions } from "./service.js";
+import { NodeAudioBufferPlayer } from "./ui/audio/node-cli/node-audiobuffer-player.js";
+import {
+  FetchAndPopulateBuffers,
+  ParseTextToSpeechText,
+  TTextToSpeechOptions,
+} from "./ui/audio/text-to-speech.js";
+import { WaitUntilInteracted } from "./ui/audio/web/wait-until-interacted.js";
+import { WebAudioBufferPlayer } from "./ui/audio/web/web-audiobuffer-player.js";
+
+await WaitUntilInteracted();
+
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+const AonyxBuddyState : Record<string, any> = {
+  eventCallback : (e: unknown) => {
+    console.log("EventCallback", e);
+  },
+};
+
+if (typeof window !== "undefined") {
+  AonyxBuddyState.ctx = new AudioContext();
+  AonyxBuddyState.analyser = AonyxBuddyState.ctx.createAnalyser();
+  AonyxBuddyState.analyser.connect(AonyxBuddyState.ctx.destination);
+  AonyxBuddyState.inputEmitter = window;
+  AonyxBuddyState.soundPlayer = WebAudioBufferPlayer;
+}else {
+  AonyxBuddyState.inputEmitter = new EventTarget();
+  AonyxBuddyState.soundPlayer = NodeAudioBufferPlayer;
+}
 
 const options: TAonyxBuddyWebClientOptions = {
   logger: console,
   streamEventService: {
     logger: console,
-    callback: (e) => console.log("SES",e),
-    inputEmitter: window,
+    callback: AonyxBuddyState.eventCallback,
+    inputEmitter: AonyxBuddyState.inputEmitter,
   },
   streamElementsOptions: {
     logger: console,
-    callback: e => console.log("SE", e),
-    inputEmitter: window,
+    callback: AonyxBuddyState.eventCallback,
+    inputEmitter: AonyxBuddyState.inputEmitter,
   },
   streamElementsSocketOptions: {
     logger: console,
     listenToTestEvents: true,
     getJWT: async () => {
-      if (!ObjectContainsKey(import.meta, "env")) throw new Error("env not found");
-      const token =  (import.meta as unknown as {env:{[key: string]: string}}).env.VITE_SE_JWT ?? "";
+      if (!ObjectContainsKey(import.meta, "env"))
+        throw new Error("env not found");
+      const token =
+        (import.meta as unknown as { env: { [key: string]: string } }).env
+          .VITE_SE_JWT ?? "";
       return token;
     },
-    callback: e => console.log("SES", e),
+    callback: AonyxBuddyState.eventCallback,
     outputEmitter: undefined,
   },
 };
